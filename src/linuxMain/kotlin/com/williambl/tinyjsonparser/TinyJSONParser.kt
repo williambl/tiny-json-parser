edc4ecad13a1f0f609edf647314a51253d7bc082
williambl <william@williambl.com>
2020-06-29 21:34:16 +0100

    Add basic parser

--- src/linuxMain/kotlin/com/williambl/tinyjsonparser/TinyJSONParser.kt
+++ src/linuxMain/kotlin/com/williambl/tinyjsonparser/TinyJSONParser.kt
@@ -1,7 +1,51 @@
 package com.williambl.tinyjsonparser
 
-fun hello(): String = "Hello, Kotlin/Native!"
-
 fun main() {
-    println(hello())
+    val toParse = generateSequence(::readLine).joinToString("\n")
+    print(Parser(toParse).read(::isWhitespace))
+}
+
+class Parser(val input: CharSequence) {
+    var cursor: Int = 0;
+
+    fun step(): Char {
+        return input[cursor++]
+    }
+
+    fun peek(): Char {
+        return input[cursor]
+    }
+
+    fun skip() {
+        cursor++
+    }
+
+    fun read(predicate: (Char) -> Boolean): String {
+        val result = StringBuilder()
+        while (predicate(peek())) {
+            result.append(step())
+        }
+        return result.toString()
+    }
+
+    fun read(pattern: CharSequence): String {
+        val result = StringBuilder()
+        var patternCursor = 0
+        while (peek() == pattern[patternCursor++]) {
+            result.append(skip())
+        }
+        return result.toString()
+    }
+
+    //TODO: readBoolean
+
+    //TOOO: readNull
+
+    //TODO: readNumber
+
+    //TODO: readString
+}
+
+fun isWhitespace(input: Char): Boolean {
+    return input == ' ' || input == '\r' || input == '\n' || input == '\t'
 }
